7c887c270c01d7389088cf0ecc36433c2ea9515c
princesachdeva20 <52614846+princesachdeva20@users.noreply.github.com>
2023-03-24 18:36:04 -0400

    Inheritance learn

--- UseInheritance.java
+++ UseInheritance.java
@@ -1,2 +1,57 @@
-package PACKAGE_NAME;public class UseInheritance {
+class creatures {
+
+    //declare private members here
+    public int age;
+    public String name;
+    void set_data(int a, String b) {
+        //initialize members here
+        this.age = a;
+        this.name = b;
+    }
+    public creatures(){
+        this.age =0;
+        this.name = null;
+    }
+    //implement getters here
+    public int getAge(){
+        return age;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getAnimalDetails(){
+        return ("The " + getClass().getSimpleName() + " named " + name + " is " + age + " years old.");
+    }
+}
+
+//define derived class named "Zebra" here
+class Zebra extends creatures {
+    final String origin = "Africa";
+    String message_zebra() {
+        //define here
+        return (getAnimalDetails()+ " The "+ getClass().getSimpleName()+" comes from " + origin); //update this when you write your code
+
+    }
+}
+
+//define derived class named "Dolphin" here
+
+class Dolphin extends creatures {
+    final String origin = "New Zealand";
+    String message_dolphin() {
+        //define here
+       return (getAnimalDetails()+" The "+ getClass().getSimpleName()+" comes from " + origin); //update this when you write your code
+
+    }
+}
+public class UseInheritance {
+    public static void main(String [] args){
+        Zebra Jin = new Zebra();
+        Jin.set_data(4,"jin");
+        System.out.println(Jin.message_zebra());
+
+        Dolphin Ann = new Dolphin();
+        Ann.set_data(2,"Ann");
+        System.out.println(Ann.message_dolphin());
+    }
 }
