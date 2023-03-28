26354eab49cf6ff9b44a5024eff013b680055410
princesachdeva20 <52614846+princesachdeva20@users.noreply.github.com>
2023-03-28 00:07:46 -0400

    new files

---
+++ primeFactors.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class primeFactors {
+    public static void main (String[] args){
+        int number;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an Integer");
+        number = input.nextInt();
+
+        // Get Prime Factors
+        for (int i =2; i<=number;i++){
+            while (number % i ==0){
+
+            }
+        }
+
+
+    }
+}
